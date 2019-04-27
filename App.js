ae09664ec050be9ae1a145e9619370c24382f773
Pavel <47458116+PavlikFilippich@users.noreply.github.com>
2019-04-27 16:03:15 +0300

    Нижнее подчеркивание

--- App.js
+++ App.js
@@ -6,39 +6,41 @@ import ThirdTab from './components/content/ThirdTab';
 
 class MyTable extends Component {
     state = {
-      currComponent: null,
+      currComponent: <FirstTab/>,
+      active:null,
     };
   
   contentOne = (ComponentName)=>{
     this.setState({currComponent: <ComponentName />});
   }
+
+  activeTd(number) {
+    this.setState({ active: number })
+  }
+
  
   render() {
     const {currComponent}=this.state;
     return(
-      <div>
-      <button
-        className = "buttonText"
-        onClick={ () => this.contentOne(FirstTab)}>
-          <span>One item</span>
-        </button>
-
-        <button
-        className = "buttonText"
-        onClick={ () => this.contentOne(SecondTab)}
-        >
-          <span>Two item</span>
-        </button>
-
-        <button
-         className = "buttonText"
-         onClick={ () => this.contentOne(ThirdTab)}>
-          <span>Three item</span>
-        </button>
-        <div className="content">
-         {currComponent}
-        </div>
-      </div>
+      <table>
+        <tbody>
+        <tr className="trButton">
+          <td
+          className={this.state.active === 1 ? 'active' : 'buttonText'}
+          onClick={ () => {this.contentOne(FirstTab) ; this.activeTd(1)}}
+          >One item</td>
+          <td
+          className={this.state.active === 2 ? 'active' : 'buttonText'}
+          onClick={ () => {this.contentOne(SecondTab);this.activeTd(2)}}
+          >Two item</td>
+          <td
+          className={this.state.active === 3 ? 'active' : 'buttonText'}
+          onClick={ () => {this.contentOne(ThirdTab);this.activeTd(3)}}
+          >Three item</td>
+        </tr>
+        <tr className="content">{currComponent}</tr>
+        </tbody>
+      </table>
     )
   }
 }
